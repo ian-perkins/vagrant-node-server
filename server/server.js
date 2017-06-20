7976f5b577ff7dd855a9da30fcb5c8af48cda582
Ian Perkins <ian@ianperkins.io>
2017-06-20 12:15:53 +0100

    Add basic error handling middleware

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = 8081;
 
 // Create the Express server
-const app = express();
+const app = express(); 
 
 // Establish where the static files are served from
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -14,13 +14,19 @@ app.get('/', (req, res) => {
   res.status(200).json({ data: { msg: 'Welcome to your Express powered API server' } });
 });
 
-// Error handler
-app.use((err, req, res, next) => {
-  console.log('An error has happened');
-  console.log(err);
-  res.status(500).end();
+// Getting here means the URL didn't match any defined route
+app.use((req, res, next) => {
+  let err = new Error(`URL ${req.path} not found`);
+  err.status = 404;
+  next(err);
 });
 
+// Error handler - catch any thrown error
+app.use((err, req, res, next) => {
+  res.status(err.status).json({ data: { msg: err.message } });
+});
+
+// Let's get started...
 app.listen(PORT, _ => {
   console.log(`App is listening on port ${PORT}`);
 });
